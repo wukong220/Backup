687e8dff5f1299831d71df98a3d6ed6db44c19f9
wukong220 <42033284+wukong220@users.noreply.github.com>
2019-05-22 11:59:08 +0800

    Add files via upload

---
+++ Codes/circles.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <time.h>
+#include <string.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <cstdlib>
+using namespace std;
+
+#define max(x, y)  ( x >= y? x : y )
+#define min(x, y)  ( x <= y? x : y )
+int number(double a, double b, double l, double phi);				//Number of circles with dencity of phi 
+void now(ofstream* fout);
+const double PI=3.1415926535897;	//pi
+
+const int Type_circles = 1;				//atom type of circlestacles
+const int MID_circles = 1;				//Mollecule ID of circles
+
+const double L_box = 70.0;					//Lengh of the box
+
+//chain setup
+const double rad = 0.5;						//Radius of the chain
+const double Sigma = 2 * rad;				//2 * radius of the chain * 2
+const int N_chain = 30;						//Polarization of single chain
+const int Num_chains = 1;					//Number of the chains
+
+//circlestacle setup
+const double rad_circles = 5.0;									//Radius of the circlestacle
+const double R_circles = 2 * rad_circles ;							//2 * radius of the circlestacle * 2
+const double Phi_circles = 0.58;									//Area density of the circlestacles
+const double inter = Sigma + 2.5 * rad_circles;					//Minimal distace between beeds and circlestacles
+const int Num_circles = number(rad_circles, rad_circles, L_box, Phi_circles);		//Number of the circlestacles
+
+const int Num_atoms = Num_circles;			//Number of atoms
+const int Num_bonds = 0;						//Number of bonds
+const int Num_angles = 0;						//Number of angles
+const int N_atomtypes = 1;						//Number of atom types
+const int N_bondtypes = 0;						//Number of bond types
+const int N_angltypes = 0;						//Number of angle types
+	
+struct Ranq2
+{
+	unsigned long long v, w;
+	Ranq2(unsigned long long j) : v(4101842887655102017LL), w(1) {
+		v ^= j;
+		w = int64();
+		v = int64();
+	}
+	inline unsigned long long int64() {
+		v ^= v >> 17; v ^= v << 31; v ^= v >> 8;
+		w = 4294957665U * (w & 0xffffffff) + (w >> 32);
+		return v ^ w;
+	}
+	inline double doub() { return 5.42101086242752217E-20 * int64(); }
+	inline unsigned int int32() { return (unsigned int)int64(); }
+};
+struct Normaldev : Ranq2 {
+	double mu, sig;
+	Normaldev(double mmu, double ssig, unsigned long long i) : Ranq2(i), mu(mmu), sig(ssig) {}
+	double dev();	//	Return a normal deviate.
+};
+
+
+int main()
+{
+	double dx;
+	double dy;
+	double dr;
+	int t;
+
+	Ranq2 Rand((unsigned int)(time(NULL)));
+	
+	// 2 dimensional array for particles
+	double**  par = new double* [Num_circles];	
+	for (int i = 0;i < Num_circles;i++)
+			par[i] = new double[2];
+	
+	// Create Num_circles random particlar position with minimal distance inter
+	for (int i = 0; i < Num_circles;)
+	{
+		par[i][0] = (L_box - rad_circles) * Rand.doub();
+		par[i][1] = (L_box - rad_circles) * Rand.doub();
+//		if(!checkin(L_box, inter, par, i))
+			i++;
+	}
+	
+	//Output file.data
+	string filename = "circles.data";
+	ofstream fout(filename);
+
+	now(&fout);
+	fout << Num_circles << " atoms\n\n\t\t"
+	<< Num_bonds << " bonds\n\n\t\t"
+	<< Num_angles <<" angles\n\n\t\t"
+	<< N_atomtypes << " atom types\n\n\t\t"
+	<< N_bondtypes << " bond types\n\n\t\t"
+	<< N_angltypes << " angle types\n\n\t\t"
+	<< 0 << " " << L_box << " xlo xhi\n\t\t"
+	<< 0 << " " << L_box << " ylo yhi\n\t\t"
+	<< -0.5 << " " << 0.5 << " zlo zhi\n\nMasses\n\n\t"
+	<< Type_circles << "  " << 1.0 << "\n\nAtoms\n"
+	<< endl;
+		
+	//Atoms
+	for (int i = 0; i < Num_circles; i++) 
+	{
+		fout << i + 1 << "  "
+		<< MID_circles << "  "
+		<< Type_circles << "  "
+		<< par[i][0] << "    "
+		<< par[i][1] << "    "
+		<< 0 << endl;
+
+	}
+	
+	fout.close();
+		
+	for (int i=0; i < Num_circles; i++)
+		delete[] par[i];
+	delete[] par;
+	
+	return 0;
+}
+
+
+
+//	Return a normal deviate.
+double Normaldev::dev()	
+{
+	double u, v, x, y, q;
+	do {
+		u = doub();
+		v = 1.7156*(doub() - 0.5);
+		x = u - 0.449871;
+		y = abs(v) + 0.386595;
+		q = x*x + y*(0.19600*y - 0.25472*x);
+	} while (q > 0.27597 && (q > 0.27846 || v*v > -4.*log(u)*u*u));
+	return mu + sig*v / u;
+}
+
+//Number of circlestacle with radius of r, with dencity of phi, in box of l
+inline int number(double a, double b, double l, double phi)
+{
+	double s = PI * a * b;
+	double S = l * l;
+	double num = phi * S/s;
+	return int(num);
+}
+
+//add time for data file
+void now(ofstream* fout)
+{
+	time_t now = time(NULL);
+	tm *ptm = localtime(&now);
+	
+	*fout << 1900 + ptm->tm_year << "/"
+	<< 1 + ptm->tm_mon << "/"
+	<< ptm->tm_mday << " "
+	<< ptm->tm_hour << ":"
+	<< ptm->tm_min
+	<< " for LAMMPS data file from circles.cpp:" << "\n\n\t\t";
+}
---
+++ Codes/ck_msd.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <functional>
+#include <cstdlib>
+
+using namespace std;
+int Nframe=1000,Nmax=Nframe-50;
+int Npassive=30;
+int Nactive=10;
+int Ncircle=1;
+double Mp=1.0,Mc=3.0,Mt=Mp*Npassive+Mc*Nactive;
+
+main()
+{
+	int i,j,k,g,dt,type,t,index,index2,dtmax;
+	double xu,yu,mux,muy,x,y,fx,fy,vx,vy;
+	double sumx,sumy,time;
+	double M=Npassive*1+Nactive*3;
+	double *xc,*yc;
+	double *msdxc,*msdyc;
+	int *cnt;
+	string temp1,temp2,id;
+
+	xc = (double*)malloc(Nframe*Ncircle*sizeof(double));
+	yc = (double*)malloc(Nframe*Ncircle*sizeof(double));
+	cnt = (int*)malloc(Nmax*sizeof(int));
+	msdxc =(double*)malloc(Nmax*sizeof(double));
+	msdyc =(double*)malloc(Nmax*sizeof(double));
+	
+	for(i=0;i<Nmax;i++)		//init
+	{
+		cnt[i]=0;
+		msdxc[i]=0.0;
+		msdyc[i]=0.0;
+	}	
+
+	//Á£×Ó×ø±ê¶ÁÈ¡
+	ifstream infile1("poly.lammpstrj",ios::in);
+	ifstream infile2("actv.lammpstrj",ios::in);
+
+	for(i=0;i<Nframe;i++)
+	{
+		for(g=0;g<9;g++)		//the head
+		{
+			getline(infile1,temp1);
+			getline(infile2,temp2);
+		}
+
+		
+		for(j=0;j<Ncircle;j++)
+		{
+			sumx=0.0;
+			sumy=0.0;
+			for(k=0;k<Npassive;k++)
+			{
+				getline(infile1,temp1);		//read line
+				stringstream word(temp1);	
+				word>>id>>type>>xu>>yu>>x>>y>>fx>>fy>>vx>>vy;		//input
+				sumx=sumx+xu*Mp;
+				sumy=sumy+yu*Mp;
+			}
+			for(k=0;k<Nactive;k++)
+			{
+				getline(infile2,temp2);
+				stringstream word(temp2);
+				word>>id>>type>>xu>>yu>>mux>>muy>>fx>>fy>>vx>>vy;
+				sumx=sumx+xu*Mc;
+				sumy=sumy+yu*Mc;
+			}
+			index=i*Ncircle+j;
+			xc[index]=sumx/Mt;
+			yc[index]=sumy/Mt;
+		}
+
+	}
+	infile1.close();
+	infile2.close();
+
+    for(t=0;t<Nframe-1;t++)
+    {
+    	dtmax=Nmax<Nframe-t-1?Nmax:Nframe-t-1;
+		for(dt=1;dt<=dtmax;dt++)
+    	{
+	    	for(i=0;i<Ncircle;i++)
+	    	{
+		    	cnt[dt-1]++;
+		    	index=t*Ncircle+i;
+		    	index2=(t+dt)*Ncircle+i;
+				msdxc[dt-1] += (xc[index2] - xc[index])*(xc[index2] - xc[index]);
+             	msdyc[dt-1] += (yc[index2] - yc[index])*(yc[index2] - yc[index]);
+	    	}
+     	}
+    }
+
+    FILE *fp;
+	fp=fopen("msd.dat","w");
+    for(t=0;t<Nmax;t++)
+    {
+    	msdxc[t] /= cnt[t];
+        msdyc[t] /= cnt[t];
+		time=(t+1)*4000*0.001;
+		fprintf(fp,"%.7f,%.11f,%.11f,%.11f\n",time,msdxc[t],msdyc[t],msdxc[t]+msdyc[t]);
+    }
+    fclose(fp);
+	cout<<"Done!"<<endl;
+    return 0;
+}
